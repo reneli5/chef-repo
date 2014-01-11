73a889ae991db7921bff74cc36a4d7b162235d3b
reneli5 <reneli5@hotmail.com>
2014-01-11 11:23:54 +0000

    update solo.rb

--- solo.rb
+++ solo.rb
@@ -1,4 +1,4 @@
-current_dir = File.expand_path(File.dirname(_FILE_))
+current_dir = File.expand_path(File.dirname(__FILE__))
 file_cache_path "#{current_dir}"
 cookbook_path "#{current_dir}/cookbooks"
 role_path "#{current_dir}/roles"
